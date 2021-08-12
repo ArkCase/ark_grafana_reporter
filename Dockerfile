070810540f984d5d274f660c4b07d4878b9cd4a1
Diego Rivera <diego.rivera@armedia.com>
2021-08-12 10:33:43 -0600

    More progress, added the executable

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,74 @@
+FROM 345280441424.dkr.ecr.ap-south-1.amazonaws.com/ark_base:latest
 
+#
+# Basic Parameters
+#
+ARG ARCH="amd64"
+ARG OS="linux"
+ARG VER="2.3.1"
+ARG PKG="grafana-reporter"
+ARG SRC="github.com/IzakMarais/reporter/cmd/grafana-reporter"
+ARG GO_VER="1.16.7"
+ARG GO_SRC="https://golang.org/dl/go${GO_VER}.${OS}-${ARCH}.tar.gz"
+ARG UID="root"
+
+#
+# Some important labels
+#
+LABEL ORG="Armedia LLC"
+LABEL MAINTAINER="Armedia Devops Team <devops@armedia.com>"
+LABEL APP="Grafana Reporter"
+LABEL VERSION="${VER}"
+
+#
+# Set the Go environment
+#
+ENV GOROOT="/usr/local/go"
+ENV GOPATH="/go"
+ENV PATH="${PATH}:${GOROOT}/bin"
+
+WORKDIR "${GOROOT}"
+
+#
+# Install the requisite packages for the build
+#
+RUN yum -y update && \
+    yum -y install \
+        git \
+        texlive \
+        texlive-luatex \
+        texlive-xetex  \
+        perl-Tk \
+        perl-Digest-MD5
+
+#
+# Download and install go
+#
+RUN curl -L "${GO_SRC}" -o - | tar -C "/usr/local" -xzf -
+
+# Install the packages we'll need
+RUN apk add --no-cache \
+    texlive \
+    texlive-xetex \
+    texlive-luatex \
+    texmf-dist \
+    texmf-dist-latexextra \
+    texmf-dist-pictures \
+    texmf-dist-science \
+    texmf-dist-formatsextra
+
+RUN go get "${SRC}/cmd/grafana-reporter"
+RUN go install -v "${SRC}/cmd/grafana-reporter@v${VER}"
+RUN cp -vi "${GOPATH}/bin/grafana-reporter" "/usr/local/bin"
+
+#
+# Copy the executable over
+#
+COPY /startup.sh /
+RUN chmod ug+rwx,o-rwx /startup.sh
+
+USER        ${UID}
+EXPOSE      8686
+VOLUME      [ "/templates" ]
+SHELL       [ "/bin/bash", "-c" ]
+ENTRYPOINT  [ "/startup.sh" ]
---
+++ startup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+say() {
+	/bin/echo -e "${@}"
+}
+
+err() {
+	say "ERROR: ${@}" 1>&2
+}
+
+fail() {
+	err "${@}"
+	exit 1
+}
+
+PARAMS=()
+
+# -ip string
+#       Grafana IP and port. (default "localhost:3000")
+if [ -n "${GRAFANA_URL}" ] ; then
+	# TODO: Validate the host:port syntax?
+	PARAMS+=("-ip" "${GRAFANA_URL}")
+fi
+
+# -grid-layout
+#       Enable grid layout (-grid-layout=1). Panel width and height will be calculated based off Grafana gridPos width and height.
+[ -n "${GRID_LAYOUT}" ] && PARAMS+=("-grid-layout=${GRID_LAYOUT}")
+
+# -templates string
+#       Directory for custom TeX templates. (default "templates/")
+PARAMS+=("-templates" "/templates")
+
+# -proto string
+#       Grafana Protocol. Change to 'https://' if Grafana is using https. Reporter will still serve http. (default "http://")
+GRAFANA_SSL="${GRAFANA_SSL,,}"
+case "${GRAFANA_SSL}" in
+	1 | y | yes | true | t | on ) GRAFANA_SSL="true" ;;
+	0 | n | no | false | f | off ) GRAFANA_SSL="false" ;;
+	* ) GRAFANA_SSL="false" ;;
+esac
+SSL_PROTO="http://"
+${GRAFANA_SSL} && SSL_PROTO="https://"
+PARAMS+=("-proto" "${SSL_PROTO}")
+
+if "${GRAFANA_SSL}" ; then
+	# -ssl-check
+	#       Check the SSL issuer and validity. Set this to false if your Grafana serves https using an unverified, self-signed certificate. (default true)
+	SSL_CHECK="${SSL_CHECK,,}"
+	case "${SSL_CHECK}" in
+		1 | y | yes | true | t | on ) SSL_CHECK="true" ;;
+		0 | n | no | false | f | off ) SSL_CHECK="false" ;;
+		* ) SSL_CHECK="true" ;;
+	esac
+	PARAMS+=("-ssl-check" "${SSL_CHECK}")
+fi
+
+say "Executing with parameters ${PARAMS[@]}"
+exec /usr/local/bin/grafana-reporter "${PARAMS[@]}"
